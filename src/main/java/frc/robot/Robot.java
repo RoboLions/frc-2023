042c3b5c042f5d8e7344bef52651692c6d411006
katiewang22 <katie.wang1950f@gmail.com>
2023-01-31 19:37:17 -0500

    robot pose

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -33,6 +33,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     RobotMap.init();
+    RobotMap.swerve.zeroGyro();
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
