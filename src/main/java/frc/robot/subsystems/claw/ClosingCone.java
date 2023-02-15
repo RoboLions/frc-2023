d90694758861d9b5df7daca80aa803556b8fb146
katiewang22 <katie.wang1950f@gmail.com>
2023-02-15 17:13:02 -0500

    claw color sensor and states updated

---
+++ src/main/java/frc/robot/subsystems/claw/ClosingCone.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.claw;
+
+import frc.robot.RobotMap;
+import frc.robot.lib.statemachine.State;
+import frc.robot.lib.statemachine.Transition;
+
+/** Add your docs here. */
+public class ClosingCone extends State {
+    
+    @Override
+    public void build() {
+        
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/claw/ClosingCube.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.claw;
+
+import frc.robot.RobotMap;
+import frc.robot.lib.statemachine.State;
+import frc.robot.lib.statemachine.Transition;
+
+/** Add your docs here. */
+public class ClosingCube extends State {
+
+    
+    @Override
+    public void build() {
+
+    }
+    
+}
