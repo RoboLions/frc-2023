6b3608643c4a0ad4af6a3810402f7fb2da147bd9
katiewang22 <katie.wang1950f@gmail.com>
2023-03-12 15:45:06 -0400

    remove comment

--- src/main/java/frc/robot/subsystems/drive/TeleopState.java
+++ src/main/java/frc/robot/subsystems/drive/TeleopState.java
@@ -47,7 +47,6 @@ public class TeleopState extends State {
         strafeVal = MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.STRAFE_VAL), Constants.STICK_DEADBAND);
         rotationVal = MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.ROTATION_VAL), Constants.STICK_DEADBAND);
 
-        // if left trigger pressed, zero gyro
         if (RobotMap.driverController.getRawButton(Constants.DriverControls.ZERO_GYRO)) {
             RobotMap.swerve.zeroGyro();
         }
