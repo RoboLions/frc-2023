44fe011465db98d0a4a28b06657f22661f67ab5f
katiewang22 <katie.wang1950f@gmail.com>
2023-03-12 23:18:51 -0400

    profiled pid instead of holonomic drive controller

--- src/main/java/frc/robot/subsystems/drive/FollowTag.java
+++ src/main/java/frc/robot/subsystems/drive/FollowTag.java
@@ -5,10 +5,13 @@
 package frc.robot.subsystems.drive;
 
 import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 import frc.robot.lib.interfaces.Swerve;
@@ -18,7 +21,23 @@ import frc.robot.lib.statemachine.Transition;
 /** State to auto-align to a known position on the field */
 public class FollowTag extends State {
 
-    HolonomicDriveController m_controller;
+    private final ProfiledPIDController xController =
+        new ProfiledPIDController(
+            1.0, 0.0, 0.0, 
+            new TrapezoidProfile.Constraints(1.0, 1.0)
+    );
+
+    private final ProfiledPIDController yController =
+        new ProfiledPIDController(
+            1.0, 0.0, 0.0, 
+            new TrapezoidProfile.Constraints(1.0, 1.0)
+    );
+
+    private final ProfiledPIDController thetaController =
+        new ProfiledPIDController(
+            1.0, 0.0, 0.0, 
+            new TrapezoidProfile.Constraints(1.0, 1.0)
+    );
 
     @Override
     public void build() {
@@ -35,12 +54,6 @@ public class FollowTag extends State {
     public void init() {
         Pose2d currentPose = Swerve.swerveOdometry.getEstimatedPosition();
         RobotMap.swerve.setClosestPose(currentPose);
-        
-        m_controller = new HolonomicDriveController(
-            Constants.SWERVE.Profile.X_CONTROLLER,
-            Constants.SWERVE.Profile.Y_CONTROLLER,
-            Constants.SWERVE.Profile.HD_THETA_CONTROLLER
-        );
     }
 
     @Override
@@ -50,12 +63,37 @@ public class FollowTag extends State {
         } else if (RobotMap.driverController.getRawButton(Constants.DriverControls.SHIFT_RIGHT_BUTTON)) {
             RobotMap.swerve.shiftPoseRight();
         }
-        Pose2d targetPose = RobotMap.swerve.getClosestPose();
-        Pose2d currentPose = Swerve.swerveOdometry.getEstimatedPosition();
-        ChassisSpeeds targetChassisSpeeds = m_controller.calculate(currentPose, targetPose, 0.25, targetPose.getRotation());
-        SwerveModuleState[] outputModuleStates = Constants.SWERVE.SWERVE_KINEMATICS.toSwerveModuleStates(targetChassisSpeeds);
 
-        RobotMap.swerve.setModuleStates(outputModuleStates);
+        double targetPoseX = RobotMap.swerve.getClosestPose().getX();
+        double targetPoseY = RobotMap.swerve.getClosestPose().getY();
+        double targetPoseRotation = RobotMap.swerve.getClosestPose().getRotation().getDegrees();
+
+        double currentPoseX = Swerve.swerveOdometry.getEstimatedPosition().getX();
+        double currentPoseY = Swerve.swerveOdometry.getEstimatedPosition().getY();
+        double currentPoseRotation = Swerve.swerveOdometry.getEstimatedPosition().getRotation().getDegrees();
+
+        double translationVal = xController.calculate(currentPoseX, targetPoseX);
+        double strafeVal = yController.calculate(currentPoseY, targetPoseY);
+        double rotationVal = thetaController.calculate(currentPoseRotation, targetPoseRotation);
+
+        if (Math.abs(translationVal) < 0.08) {
+            translationVal = 0.0;
+        }
+
+        if (Math.abs(strafeVal) < 0.08) {
+            strafeVal = 0.0;
+        }
+
+        if (Math.abs(rotationVal) < 0.08) {
+            rotationVal = 0.0;
+        }
+
+        RobotMap.swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(1.0), // Constants.SWERVE.MAX_SPEED), 
+            rotationVal * Constants.SWERVE.MAX_ANGULAR_VELOCITY * 0.3,
+            false, 
+            true
+        );
     }
 
     @Override
