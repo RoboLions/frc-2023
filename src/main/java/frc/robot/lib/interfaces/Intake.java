e87d447819aa41d7596bbb59b8107aa87f39ad62
katiewang22 <katie.wang1950f@gmail.com>
2023-03-25 19:46:10 -0400

    remove unused timer in intake interface

--- src/main/java/frc/robot/lib/interfaces/Intake.java
+++ src/main/java/frc/robot/lib/interfaces/Intake.java
@@ -4,7 +4,6 @@
 
 package frc.robot.lib.interfaces;
 
-import edu.wpi.first.wpilibj.Timer;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
@@ -13,13 +12,10 @@ import frc.robot.subsystems.LED.LEDStateMachine;
 /** Class with methods related to the claw or color sensor */
 public class Intake {
 
-    private static Timer colorTimer = new Timer();
-
     public Intake() {
         RobotMap.intakeMotor.configPeakOutputForward(1.0);
         RobotMap.intakeMotor.configPeakOutputReverse(-1.0);
         RobotMap.intakeMotor.setInverted(true);
-        colorTimer.start();
     }
 
     public void runIntake() {
