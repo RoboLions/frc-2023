92d8e8b40fc9f45c75aeafe5de79f25762c101fa
Nate C <nguyen.n@prhsrobotics.com>
2023-07-06 18:13:57 -0400

    Added Trapezoidal for translationval and added Smart Dashboard Logging

--- src/main/java/frc/robot/subsystems/drive/TeleopState.java
+++ src/main/java/frc/robot/subsystems/drive/TeleopState.java
@@ -6,6 +6,7 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
@@ -18,9 +19,11 @@ import frc.robot.subsystems.arm.ArmStateMachine;
 
 public class TeleopState extends State {
     
-    double translationVal;
-    double strafeVal;
-    double rotationVal = 0.0;
+    private final TrapezoidProfile.Constraints m_Constraints =
+        new TrapezoidProfile.Constraints(1, 1);
+
+    private TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
+    
     double translationValScalar;
 
     double prevRotationVal = 0.0;
@@ -63,10 +66,23 @@ public class TeleopState extends State {
         //     SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);
         // }
         
-        translationVal = -1.0 * MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.TRANSLATION_VAL), Constants.STICK_DEADBAND);
-        strafeVal = -1.0 * MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.STRAFE_VAL), Constants.STICK_DEADBAND);
-        rotationVal = MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.ROTATION_VAL), Constants.STICK_DEADBAND);
+        double translationVal = -1.0 * MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.TRANSLATION_VAL), Constants.STICK_DEADBAND);
+        double strafeVal = -1.0 * MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.STRAFE_VAL), Constants.STICK_DEADBAND);
+        double rotationVal = MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.ROTATION_VAL), Constants.STICK_DEADBAND);
 
+        TrapezoidProfile.State m_goal = new TrapezoidProfile.State(translationVal, 0.0);
+        // Create a motion profile with the given maximum velocity and maximum
+        // acceleration constraints for the next setpoint, the desired goal, and the
+        // current setpoint.
+        var profile = new TrapezoidProfile(m_Constraints, m_goal, m_setpoint);
+
+        // Retrieve the profiled setpoint for the next timestep. This setpoint moves
+        // toward the goal while obeying the constraints.
+        m_setpoint = profile.calculate(0.02);
+
+        translationVal = m_setpoint.position;
+
+        SmartDashboard.putNumber("translationVal", translationVal);
         // if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
         //     translationVal = MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.TRANSLATION_VAL), Constants.STICK_DEADBAND);
         //     strafeVal = MathUtil.applyDeadband(-RobotMap.driverController.getRawAxis(Constants.DriverControls.STRAFE_VAL), Constants.STICK_DEADBAND);
@@ -137,7 +153,7 @@ public class TeleopState extends State {
             new Translation2d(translationVal, strafeVal).times(translationValScalar), 
             rotationVal,
             true, 
-            true
+            false
         );
     }
 
